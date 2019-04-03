287af2cedac7864a6711d72ce8f9e3fde96de2ef
Tara Sankhalpara <tsankhalpara@mac.com>
2019-04-03 12:27:19 +0100

    add intial locked diary and locked? status

--- lib/secret_diary.rb
+++ lib/secret_diary.rb
@@ -1,17 +1,29 @@
 class SecretDiary
 
+  def initialize
+    @locked = true
+  end
+
   def lock
+    @locked = true
     "Diary is locked"
   end
 
   def unlock
+    @locked = false
     "Diary is unlocked"
   end
 
   def add_entry
+    "Entry added"
   end
 
   def get_entries
+    "Entry retrieved"
+  end
+
+  def locked?
+    @locked
   end
 
 end
